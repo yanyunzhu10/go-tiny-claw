a1d674ea751706c963111fdaec3c76f2e5de6774
yanyunzhu10 <zyy731804840@gmail.com>
2026-06-20 19:30:04 +0800

    lesson7

--- cmd/claw/main.go
+++ cmd/claw/main.go
@@ -21,20 +21,25 @@ func main() {
     llmProvider := provider.NewZhipuOpenAIProvider("glm-4.5-air")
     registry := tools.NewRegistry()
 
-    // 挂载极简工具集
+    // 挂载工具全家桶
     registry.Register(tools.NewReadFileTool(workDir))
     registry.Register(tools.NewWriteFileTool(workDir))
     registry.Register(tools.NewBashTool(workDir))
 
-    // 实例化核心引擎，关闭慢思考阶段，享受 YOLO 急速模式
+    // 【新增挂载】
+    registry.Register(tools.NewEditFileTool(workDir))
+
+    // 实例化引擎，开启 EnableThinking = true
     eng := engine.NewAgentEngine(llmProvider, registry, workDir, false)
 
-    // 发起一个需要连贯物理动作的任务
+    // 发起一个需要局部修改的指令
     prompt := `
-    请帮我执行以下操作：
-    1. 用 bash 查看一下我当前电脑的 Go 版本。
-    2. 帮我写一个简单的 helloworld.go 文件，输出 "Hello, go-tiny-claw!"。
-    3. 用 bash 编译并运行这个 go 文件，确认它能正常工作。
+    我当前目录下有一个 server.go 文件。
+    请帮我把里面 "TODO: 增加鉴权逻辑" 下面的那个 if 语句，整个替换为：
+    if user == nil {
+        fmt.Println("Forbidden!")
+        return
+    }
     `
 
     err := eng.Run(context.Background(), prompt)
